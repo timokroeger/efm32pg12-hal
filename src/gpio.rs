5dec92f9e17c0df198110f3f35067ac3379456fb
Timo Kröger <timokroeger93@gmail.com>
2019-10-27 22:18:24 +0100

    Implement the embedded-halt ToggleableOutputPin trait for all outputs

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,6 +1,6 @@
 //! General purpose I/O (GPIO) pin API
 use crate::{
-    hal::digital::v2::{InputPin, OutputPin, StatefulOutputPin},
+    hal::digital::v2::{InputPin, OutputPin, StatefulOutputPin, ToggleableOutputPin},
     pac::{
         // Each GPIO has it’s own enum for the mode field even though the values are
         // shared by all GPIOs. Import the first to have nice names for the numeric
@@ -63,6 +63,7 @@ pub trait PinTrait {
     fn clear_dout_bit(&mut self);
     fn set_dout_bit(&mut self);
     fn read_dout_bit(&self) -> bool;
+    fn write_douttgl_bit(&mut self);
     fn read_din_bit(&self) -> bool;
 }
 
@@ -79,6 +80,7 @@ macro_rules! gpios {
         $mode_reg:ident,
         $mode_field:ident,
         $dout_reg:ident,
+        $douttgl_reg:ident,
         $din_reg:ident,
         $pin_nr:expr,
         $mode:ident;
@@ -133,6 +135,11 @@ macro_rules! gpios {
                     dout_reg.read().bits() & (1 << $pin_nr) == 1 << $pin_nr
                 }
 
+                fn write_douttgl_bit(&mut self) {
+                    let douttgl = unsafe { &(*GPIO::ptr()).$douttgl_reg };
+                    douttgl.write(|w| unsafe { w.bits(1 << $pin_nr) });
+                }
+
                 fn read_din_bit(&self) -> bool {
                     let din_reg = unsafe { &(*GPIO::ptr()).$din_reg };
                     din_reg.read().bits() & (1 << $pin_nr) == 1 << $pin_nr
@@ -143,78 +150,78 @@ macro_rules! gpios {
 }
 
 gpios!(
-    pa0, PA0, pa_model, mode0, pa_dout, pa_din, 0, Disabled;
-    pa1, PA1, pa_model, mode1, pa_dout, pa_din, 1, Disabled;
-    pa2, PA2, pa_model, mode2, pa_dout, pa_din, 2, Disabled;
-    pa3, PA3, pa_model, mode3, pa_dout, pa_din, 3, Disabled;
-    pa4, PA4, pa_model, mode4, pa_dout, pa_din, 4, Disabled;
-    pa5, PA5, pa_model, mode5, pa_dout, pa_din, 5, Disabled;
-    pa6, PA6, pa_model, mode6, pa_dout, pa_din, 6, Disabled;
-    pa7, PA7, pa_model, mode7, pa_dout, pa_din, 7, Disabled;
-    pa8, PA8, pa_modeh, mode8, pa_dout, pa_din, 8, Disabled;
-    pa9, PA9, pa_modeh, mode9, pa_dout, pa_din, 9, Disabled;
+    pa0, PA0, pa_model, mode0, pa_dout, pa_douttgl, pa_din, 0, Disabled;
+    pa1, PA1, pa_model, mode1, pa_dout, pa_douttgl, pa_din, 1, Disabled;
+    pa2, PA2, pa_model, mode2, pa_dout, pa_douttgl, pa_din, 2, Disabled;
+    pa3, PA3, pa_model, mode3, pa_dout, pa_douttgl, pa_din, 3, Disabled;
+    pa4, PA4, pa_model, mode4, pa_dout, pa_douttgl, pa_din, 4, Disabled;
+    pa5, PA5, pa_model, mode5, pa_dout, pa_douttgl, pa_din, 5, Disabled;
+    pa6, PA6, pa_model, mode6, pa_dout, pa_douttgl, pa_din, 6, Disabled;
+    pa7, PA7, pa_model, mode7, pa_dout, pa_douttgl, pa_din, 7, Disabled;
+    pa8, PA8, pa_modeh, mode8, pa_dout, pa_douttgl, pa_din, 8, Disabled;
+    pa9, PA9, pa_modeh, mode9, pa_dout, pa_douttgl, pa_din, 9, Disabled;
 
-    pb6, PB6, pb_model, mode6, pb_dout, pb_din, 6, Disabled;
-    pb7, PB7, pb_model, mode7, pb_dout, pb_din, 7, Disabled;
-    pb8, PB8, pb_modeh, mode8, pb_dout, pb_din, 8, Disabled;
-    pb9, PB9, pb_modeh, mode9, pb_dout, pb_din, 9, Disabled;
-    pb10, PB10, pb_modeh, mode10, pb_dout, pb_din, 10, Disabled;
-    pb11, PB11, pb_modeh, mode11, pb_dout, pb_din, 11, Disabled;
-    pb12, PB12, pb_modeh, mode12, pb_dout, pb_din, 12, Disabled;
-    pb13, PB13, pb_modeh, mode13, pb_dout, pb_din, 13, Disabled;
-    pb14, PB14, pb_modeh, mode14, pb_dout, pb_din, 14, Disabled;
-    pb15, PB15, pb_modeh, mode15, pb_dout, pb_din, 15, Disabled;
+    pb6, PB6, pb_model, mode6, pb_dout, pb_douttgl, pb_din, 6, Disabled;
+    pb7, PB7, pb_model, mode7, pb_dout, pb_douttgl, pb_din, 7, Disabled;
+    pb8, PB8, pb_modeh, mode8, pb_dout, pb_douttgl, pb_din, 8, Disabled;
+    pb9, PB9, pb_modeh, mode9, pb_dout, pb_douttgl, pb_din, 9, Disabled;
+    pb10, PB10, pb_modeh, mode10, pb_dout, pb_douttgl, pb_din, 10, Disabled;
+    pb11, PB11, pb_modeh, mode11, pb_dout, pb_douttgl, pb_din, 11, Disabled;
+    pb12, PB12, pb_modeh, mode12, pb_dout, pb_douttgl, pb_din, 12, Disabled;
+    pb13, PB13, pb_modeh, mode13, pb_dout, pb_douttgl, pb_din, 13, Disabled;
+    pb14, PB14, pb_modeh, mode14, pb_dout, pb_douttgl, pb_din, 14, Disabled;
+    pb15, PB15, pb_modeh, mode15, pb_dout, pb_douttgl, pb_din, 15, Disabled;
 
-    pc0, PC0, pc_model, mode0, pc_dout, pc_din, 0, Disabled;
-    pc1, PC1, pc_model, mode1, pc_dout, pc_din, 1, Disabled;
-    pc2, PC2, pc_model, mode2, pc_dout, pc_din, 2, Disabled;
-    pc3, PC3, pc_model, mode3, pc_dout, pc_din, 3, Disabled;
-    pc4, PC4, pc_model, mode4, pc_dout, pc_din, 4, Disabled;
-    pc5, PC5, pc_model, mode5, pc_dout, pc_din, 5, Disabled;
-    pc6, PC6, pc_model, mode6, pc_dout, pc_din, 6, Disabled;
-    pc7, PC7, pc_model, mode7, pc_dout, pc_din, 7, Disabled;
-    pc8, PC8, pc_modeh, mode8, pc_dout, pc_din, 8, Disabled;
-    pc9, PC9, pc_modeh, mode9, pc_dout, pc_din, 9, Disabled;
-    pc10, PC10, pc_modeh, mode10, pc_dout, pc_din, 10, Disabled;
-    pc11, PC11, pc_modeh, mode11, pc_dout, pc_din, 11, Disabled;
+    pc0, PC0, pc_model, mode0, pc_dout, pc_douttgl, pc_din, 0, Disabled;
+    pc1, PC1, pc_model, mode1, pc_dout, pc_douttgl, pc_din, 1, Disabled;
+    pc2, PC2, pc_model, mode2, pc_dout, pc_douttgl, pc_din, 2, Disabled;
+    pc3, PC3, pc_model, mode3, pc_dout, pc_douttgl, pc_din, 3, Disabled;
+    pc4, PC4, pc_model, mode4, pc_dout, pc_douttgl, pc_din, 4, Disabled;
+    pc5, PC5, pc_model, mode5, pc_dout, pc_douttgl, pc_din, 5, Disabled;
+    pc6, PC6, pc_model, mode6, pc_dout, pc_douttgl, pc_din, 6, Disabled;
+    pc7, PC7, pc_model, mode7, pc_dout, pc_douttgl, pc_din, 7, Disabled;
+    pc8, PC8, pc_modeh, mode8, pc_dout, pc_douttgl, pc_din, 8, Disabled;
+    pc9, PC9, pc_modeh, mode9, pc_dout, pc_douttgl, pc_din, 9, Disabled;
+    pc10, PC10, pc_modeh, mode10, pc_dout, pc_douttgl, pc_din, 10, Disabled;
+    pc11, PC11, pc_modeh, mode11, pc_dout, pc_douttgl, pc_din, 11, Disabled;
 
-    pd8, PD8, pd_modeh, mode8, pd_dout, pd_din, 8, Disabled;
-    pd9, PD9, pd_modeh, mode9, pd_dout, pd_din, 9, Disabled;
-    pd10, PD10, pd_modeh, mode10, pd_dout, pd_din, 10, Disabled;
-    pd11, PD11, pd_modeh, mode11, pd_dout, pd_din, 11, Disabled;
-    pd12, PD12, pd_modeh, mode12, pd_dout, pd_din, 12, Disabled;
-    pd13, PD13, pd_modeh, mode13, pd_dout, pd_din, 13, Disabled;
-    pd14, PD14, pd_modeh, mode14, pd_dout, pd_din, 14, Disabled;
-    pd15, PD15, pd_modeh, mode15, pd_dout, pd_din, 15, Disabled;
+    pd8, PD8, pd_modeh, mode8, pd_dout, pd_douttgl, pd_din, 8, Disabled;
+    pd9, PD9, pd_modeh, mode9, pd_dout, pd_douttgl, pd_din, 9, Disabled;
+    pd10, PD10, pd_modeh, mode10, pd_dout, pd_douttgl, pd_din, 10, Disabled;
+    pd11, PD11, pd_modeh, mode11, pd_dout, pd_douttgl, pd_din, 11, Disabled;
+    pd12, PD12, pd_modeh, mode12, pd_dout, pd_douttgl, pd_din, 12, Disabled;
+    pd13, PD13, pd_modeh, mode13, pd_dout, pd_douttgl, pd_din, 13, Disabled;
+    pd14, PD14, pd_modeh, mode14, pd_dout, pd_douttgl, pd_din, 14, Disabled;
+    pd15, PD15, pd_modeh, mode15, pd_dout, pd_douttgl, pd_din, 15, Disabled;
 
-    pf0, PF0, pf_model, mode0, pf_dout, pf_din, 0, Debug;
-    pf1, PF1, pf_model, mode1, pf_dout, pf_din, 1, Debug;
-    pf2, PF2, pf_model, mode2, pf_dout, pf_din, 2, Debug;
-    pf3, PF3, pf_model, mode3, pf_dout, pf_din, 3, Debug;
-    pf4, PF4, pf_model, mode4, pf_dout, pf_din, 4, Disabled;
-    pf5, PF5, pf_model, mode5, pf_dout, pf_din, 5, Disabled;
-    pf6, PF6, pf_model, mode6, pf_dout, pf_din, 6, Disabled;
-    pf7, PF7, pf_model, mode7, pf_dout, pf_din, 7, Disabled;
-    pf8, PF8, pf_modeh, mode8, pf_dout, pf_din, 8, Disabled;
-    pf9, PF9, pf_modeh, mode9, pf_dout, pf_din, 9, Disabled;
-    pf10, PF10, pf_modeh, mode10, pf_dout, pf_din, 10, Disabled;
-    pf11, PF11, pf_modeh, mode11, pf_dout, pf_din, 11, Disabled;
-    pf12, PF12, pf_modeh, mode12, pf_dout, pf_din, 12, Disabled;
-    pf13, PF13, pf_modeh, mode13, pf_dout, pf_din, 13, Disabled;
-    pf14, PF14, pf_modeh, mode14, pf_dout, pf_din, 14, Disabled;
-    pf15, PF15, pf_modeh, mode15, pf_dout, pf_din, 15, Disabled;
+    pf0, PF0, pf_model, mode0, pf_dout, pf_douttgl, pf_din, 0, Debug;
+    pf1, PF1, pf_model, mode1, pf_dout, pf_douttgl, pf_din, 1, Debug;
+    pf2, PF2, pf_model, mode2, pf_dout, pf_douttgl, pf_din, 2, Debug;
+    pf3, PF3, pf_model, mode3, pf_dout, pf_douttgl, pf_din, 3, Debug;
+    pf4, PF4, pf_model, mode4, pf_dout, pf_douttgl, pf_din, 4, Disabled;
+    pf5, PF5, pf_model, mode5, pf_dout, pf_douttgl, pf_din, 5, Disabled;
+    pf6, PF6, pf_model, mode6, pf_dout, pf_douttgl, pf_din, 6, Disabled;
+    pf7, PF7, pf_model, mode7, pf_dout, pf_douttgl, pf_din, 7, Disabled;
+    pf8, PF8, pf_modeh, mode8, pf_dout, pf_douttgl, pf_din, 8, Disabled;
+    pf9, PF9, pf_modeh, mode9, pf_dout, pf_douttgl, pf_din, 9, Disabled;
+    pf10, PF10, pf_modeh, mode10, pf_dout, pf_douttgl, pf_din, 10, Disabled;
+    pf11, PF11, pf_modeh, mode11, pf_dout, pf_douttgl, pf_din, 11, Disabled;
+    pf12, PF12, pf_modeh, mode12, pf_dout, pf_douttgl, pf_din, 12, Disabled;
+    pf13, PF13, pf_modeh, mode13, pf_dout, pf_douttgl, pf_din, 13, Disabled;
+    pf14, PF14, pf_modeh, mode14, pf_dout, pf_douttgl, pf_din, 14, Disabled;
+    pf15, PF15, pf_modeh, mode15, pf_dout, pf_douttgl, pf_din, 15, Disabled;
 
-    pi0, PI0, pi_model, mode0, pi_dout, pi_din, 0, Disabled;
-    pi1, PI1, pi_model, mode1, pi_dout, pi_din, 1, Disabled;
-    pi2, PI2, pi_model, mode2, pi_dout, pi_din, 2, Disabled;
-    pi3, PI3, pi_model, mode3, pi_dout, pi_din, 3, Disabled;
+    pi0, PI0, pi_model, mode0, pi_dout, pi_douttgl, pi_din, 0, Disabled;
+    pi1, PI1, pi_model, mode1, pi_dout, pi_douttgl, pi_din, 1, Disabled;
+    pi2, PI2, pi_model, mode2, pi_dout, pi_douttgl, pi_din, 2, Disabled;
+    pi3, PI3, pi_model, mode3, pi_dout, pi_douttgl, pi_din, 3, Disabled;
 
-    pj14, PJ14, pj_modeh, mode14, pj_dout, pj_din, 14, Disabled;
-    pj15, PJ15, pj_modeh, mode15, pj_dout, pj_din, 15, Disabled;
+    pj14, PJ14, pj_modeh, mode14, pj_dout, pj_douttgl, pj_din, 14, Disabled;
+    pj15, PJ15, pj_modeh, mode15, pj_dout, pj_douttgl, pj_din, 15, Disabled;
 
-    pk0, PK0, pk_model, mode0, pk_dout, pk_din, 0, Disabled;
-    pk1, PK1, pk_model, mode1, pk_dout, pk_din, 1, Disabled;
-    pk2, PK2, pk_model, mode2, pk_dout, pk_din, 2, Disabled;
+    pk0, PK0, pk_model, mode0, pk_dout, pk_douttgl, pk_din, 0, Disabled;
+    pk1, PK1, pk_model, mode1, pk_dout, pk_douttgl, pk_din, 1, Disabled;
+    pk2, PK2, pk_model, mode2, pk_dout, pk_douttgl, pk_din, 2, Disabled;
 );
 
 /// Extension trait to use the peripheral bit set and clear feature.
@@ -470,5 +477,18 @@ where
 
     fn is_set_high(&self) -> Result<bool, Self::Error> {
         Ok(self.ty.read_dout_bit())
+    }
+}
+
+impl<T, OM> ToggleableOutputPin for Pin<T, Output<OM>>
+where
+    T: PinTrait,
+    OM: OutputMode,
+{
+    type Error = ();
+
+    fn toggle(&mut self) -> Result<(), Self::Error> {
+        self.ty.write_douttgl_bit();
+        Ok(())
     }
 }
