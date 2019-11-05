70494e8ec277ce1fa00141e2d6433ea1e3f16d4c
Timo Kröger <timokroeger93@gmail.com>
2019-11-05 18:09:09 +0100

    Implement the clock enable trait for all low frequency peripherals

--- src/cmu.rs
+++ src/cmu.rs
@@ -8,9 +8,7 @@ pub trait CmuExt {
 impl CmuExt for CMU {
     // TODO: Make this configurable
     fn freeze(self) -> Cmu {
-        Cmu {
-            raw: self,
-        }
+        Cmu { raw: self }
     }
 }
 
@@ -40,8 +38,6 @@ macro_rules! impl_clock_control_ext {
 
 impl_clock_control_ext!(CRYPTO0, hfbusclken0, crypto0);
 impl_clock_control_ext!(CRYPTO1, hfbusclken0, crypto1);
-// TODO: Enable this to access the register of LF peripherals.
-// impl_clock_control_ext!(???, hfbusclken0, le);
 impl_clock_control_ext!(GPIO, hfbusclken0, gpio);
 impl_clock_control_ext!(PRS, hfbusclken0, prs);
 impl_clock_control_ext!(LDMA, hfbusclken0, ldma);
@@ -63,5 +59,30 @@ impl_clock_control_ext!(CRYOTIMER, hfperclken0, cryotimer);
 impl_clock_control_ext!(ADC0, hfperclken0, adc0);
 impl_clock_control_ext!(IDAC0, hfperclken0, idac0);
 impl_clock_control_ext!(VDAC0, hfperclken0, vdac0);
-impl_clock_control_ext!(CSEN, hfperclken0, csen);
 impl_clock_control_ext!(TRNG0, hfperclken0, trng0);
+
+macro_rules! impl_lf_clock_control_ext {
+    ($type:ty, $reg:ident, $bit:ident) => {
+        impl ClockControlExt for $type {
+            fn enable_clock(&self, clocks: &mut Cmu) {
+                // To access LF peripheral registers the HFBUSCLKLE clock must be enabled.
+                clocks.raw.hfbusclken0.modify(|_, w| w.le().set_bit());
+                clocks.raw.$reg.modify(|_, w| w.$bit().set_bit());
+            }
+        }
+    };
+}
+
+impl_lf_clock_control_ext!(LETIMER0, lfaclken0, letimer0);
+impl_lf_clock_control_ext!(LESENSE, lfaclken0, lesense);
+impl_lf_clock_control_ext!(SYST, lfbclken0, systick);
+impl_lf_clock_control_ext!(LEUART0, lfbclken0, leuart0);
+impl_lf_clock_control_ext!(RTCC, lfeclken0, rtcc);
+
+// The CSEN peripheral is special because it uses the HF and LF clock domain.
+impl ClockControlExt for CSEN {
+    fn enable_clock(&self, clocks: &mut Cmu) {
+        clocks.raw.hfperclken0.modify(|_, w| w.csen().set_bit());
+        clocks.raw.lfbclken0.modify(|_, w| w.csen().set_bit());
+    }
+}
