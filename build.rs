390a4a5859f8954588ee1932e2a66be1704d5513
Timo Kröger <timokroeger93@gmail.com>
2020-03-31 19:28:34 +0200

    Add build.rs to copy the linker file

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    fs::copy("memory.x", out.join("memory.x")).unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=memory.x");
+}
